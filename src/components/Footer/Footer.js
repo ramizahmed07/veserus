4e53bdd46887aeec26eb4b535ae7a5f9b616fb95
Ramiz Ahmed <ramiz.ahmed@letspingup.com>
2025-03-25 21:36:56 +0500

    Remove dead links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,78 +1,75 @@
-import { Link, useLocation } from "react-router-dom";
-import React from "react";
+import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
 
-import "./footer.scss";
-import PATHS from "../../router/paths";
+import './footer.scss';
+import PATHS from '../../router/paths';
 
 const FOOTER_LEFT_LINKS = [
-  { title: "Veserus", path: "/veserus" },
-  { title: "Resources", path: "/resources" },
-  { title: "Contacts", path: PATHS.CONTACT_PAGE },
-  { title: "Team", path: PATHS.TEAM },
-  { title: "Design", path: "/design" },
+  { title: 'Veserus', path: '/veserus' },
+  { title: 'Resources', path: '/resources' },
+  { title: 'Contacts', path: PATHS.CONTACT_PAGE },
+  { title: 'Team', path: PATHS.TEAM },
 ];
 
 const USER_INFORMATION_LINKS = [
-  { title: "Fees", path: PATHS.PLANS_PAGE },
-  { title: "Help Centre", path: PATHS.HELP_PAGE },
-  { title: "API Documentation", path: PATHS.API_DOCS },
+  { title: 'Fees', path: PATHS.PLANS_PAGE },
+  { title: 'API Documentation', path: PATHS.API_DOCS },
 ];
 
 const COMPANY_LINKS = [
-  { title: "About", path: PATHS.ABOUT_PAGE },
-  { title: "Professionals", path: PATHS.PROFESSIONALS },
-  { title: "Partners", path: PATHS.PARTNERS },
-  { title: "Mobile apps", path: "/mobile-apps" },
+  { title: 'About', path: PATHS.ABOUT_PAGE },
+  { title: 'Professionals', path: PATHS.PROFESSIONALS },
+  { title: 'Partners', path: PATHS.PARTNERS },
 ];
 
 const checkIfFooterPages = (pathname) => {
-  const paths = ["/professionals", "/team", "/partners", "/brand-assets"];
+  const paths = ['/professionals', '/team', '/partners', '/brand-assets'];
   return paths.includes(pathname);
 };
 
 const Footer = () => {
   const { pathname } = useLocation();
-  if (pathname.includes("/api-docs")) return null;
+  if (pathname.includes('/api-docs')) return null;
 
   return (
-    <div className="footer">
+    <div className='footer'>
       <div
         className={`footer__content ${
-          checkIfFooterPages(pathname) ? "footer__contentSM" : ""
+          checkIfFooterPages(pathname) ? 'footer__contentSM' : ''
         }`}
       >
-        <div className="footer__leftCol">
+        <div className='footer__leftCol'>
           {FOOTER_LEFT_LINKS.map(({ title, path }) => (
-            <Link to={path} key={title} className="footer__leftCol__link">
+            <Link to={path} key={title} className='footer__leftCol__link'>
               {title}
             </Link>
           ))}
         </div>
-        <div className="footer__rightCol">
-          <div className="footer__rightCol__col">
-            <div className="footer__rightCol__col__title">User Information</div>
-            <div className="footer__rightCol__col__links">
+        <div className='footer__rightCol'>
+          <div className='footer__rightCol__col'>
+            <div className='footer__rightCol__col__title'>User Information</div>
+            <div className='footer__rightCol__col__links'>
               {USER_INFORMATION_LINKS.map(({ title, path }) => (
                 <Link
                   key={path}
-                  className="footer__rightCol__col__links__link"
+                  className='footer__rightCol__col__links__link'
                   to={path}
                 >
                   {title}
                 </Link>
               ))}
             </div>
-            <p className="footer__rightCol__col__copyrights">
+            <p className='footer__rightCol__col__copyrights'>
               © 2020 Veserus Association
             </p>
           </div>
-          <div className="footer__rightCol__col">
-            <div className="footer__rightCol__col__title">Company</div>
-            <div className="footer__rightCol__col__links">
+          <div className='footer__rightCol__col'>
+            <div className='footer__rightCol__col__title'>Company</div>
+            <div className='footer__rightCol__col__links'>
               {COMPANY_LINKS.map(({ title, path }) => (
                 <Link
                   key={path}
-                  className="footer__rightCol__col__links__link"
+                  className='footer__rightCol__col__links__link'
                   to={path}
                 >
                   {title}
@@ -82,7 +79,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <p className="footer__copyrights">© 2020 Veserus Association</p>
+      <p className='footer__copyrights'>© 2020 Veserus Association</p>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
-import MenuIcon from "../../assets/images/menu.png";
-import AboutCover from "../../assets/images/aboutCover.png";
-import "./header.scss";
-import PATHS from "../../router/paths";
-import routes from "../../router/routeConfig";
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import MenuIcon from '../../assets/images/menu.png';
+import AboutCover from '../../assets/images/aboutCover.png';
+import './header.scss';
+import PATHS from '../../router/paths';
+import routes from '../../router/routeConfig';
 
 const LINKS = [
   {
-    title: "Features",
+    title: 'Features',
     path: PATHS.FEATURES_PAGE,
   },
   {
-    title: "Plans",
+    title: 'Plans',
     path: PATHS.PLANS_PAGE,
   },
   {
-    title: "About us",
+    title: 'About us',
     path: PATHS.ABOUT_PAGE,
   },
   {
-    title: "Help",
+    title: 'Help',
     path: PATHS.HELP_PAGE,
   },
   {
-    title: "Sign In",
+    title: 'Sign In',
     path: PATHS.LOGIN_PAGE,
     signIn: true,
   },
@@ -33,13 +33,13 @@ const LINKS = [
 const Header = () => {
   const [menu, toggleMenu] = useState(false);
   const { pathname } = useLocation();
-  const [color, setColor] = useState("#fff");
+  const [color, setColor] = useState('#fff');
   useEffect(() => {
     if (menu) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflow = 'hidden';
       return;
     } else {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = 'unset';
       return;
     }
   }, [menu]);
@@ -52,49 +52,49 @@ const Header = () => {
     <div
       style={{ color }}
       className={`header ${
-        pathname.includes("/contact") && "header--contact"
-      }  ${pathname.includes("/api-docs") && "header--apiDocs"}`}
+        pathname.includes('/contact') && 'header--contact'
+      }  ${pathname.includes('/api-docs') && 'header--apiDocs'}`}
     >
-      <div className={`header__overlay ${menu && "header__overlay--visible"}`}>
+      <div className={`header__overlay ${menu && 'header__overlay--visible'}`}>
         {LINKS.map(({ title, path, signIn }) => (
           <Link
             onClick={() => toggleMenu(false)}
             key={title}
             to={path}
-            className="header__overlay__listItem"
+            className='header__overlay__listItem'
           >
-            <div className="header__overlay__listItem__title">{title}</div>
-            {signIn ? <div className="circle" /> : null}
+            <div className='header__overlay__listItem__title'>{title}</div>
+            {signIn ? <div className='circle' /> : null}
           </Link>
         ))}
       </div>
-      <h2 className="header__logo">
-        <span className="header__logo--initials">ve</span>
-        <span className={`${menu && "inverse"}`}>
-          {pathname.includes("/api-docs") ? "API documentation" : "serus"}
+      <h2 className='header__logo'>
+        <span className='header__logo--initials'>ve</span>
+        <span className={`${menu && 'inverse'}`}>
+          {pathname.includes('/api-docs') ? 'API documentation' : 'serus'}
         </span>
       </h2>
-      <div className="header__links">
+      <div className='header__links'>
         {LINKS.map(({ title, path, signIn }) => (
           <Link
             key={title}
             to={path}
             className={`header__links__link ${
-              signIn && "header__links__signIn"
+              signIn && 'header__links__signIn'
             }`}
             key={title}
           >
             <span>{title}</span>
-            {signIn ? <div className="circle" /> : null}
+            {signIn ? <div className='circle' /> : null}
           </Link>
         ))}
       </div>
       <div
         onClick={() => toggleMenu((prev) => !prev)}
         className={`header__links__mobileMenu ${
-          menu && "header__links__mobileMenu--active"
+          menu && 'header__links__mobileMenu--active'
         } ${
-          pathname.includes("/api-docs") && "header__links__mobileMenu--apiDocs"
+          pathname.includes('/api-docs') && 'header__links__mobileMenu--apiDocs'
         }`}
       >
         {/* <img src={MenuIcon} alt="menu" /> */}
